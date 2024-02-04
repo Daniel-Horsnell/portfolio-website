e144450db7b548fe62281a00a77922dca376769f
Daniel-Horsnell <horsnelldaniel@gmail.com>
2024-02-04 18:10:03 +1100

    Started on the projects page.

--- project/src/components/projects.tsx
+++ project/src/components/projects.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import DensityMediumIcon from '@mui/icons-material/DensityMedium';
-import { Button } from '@mui/material';
+import React, { useState } from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { makeStyles } from '@mui/material';
 
-
+const projectsList = ['Thesis', 'Digital Twin', 'Robot Artist', 'SumoBot']
 
 function Projects() {
+  const [value, setValue] = useState<number>(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <div>
-      <h1>Projects</h1>
-      {/* Add your photos and text descriptions here */}
+      <div style={{borderRight: '1px grey', height: '100vh', width: '10vw'}}>
+        <Tabs variant='fullWidth' orientation='vertical' value={value} onChange={handleChange} sx={{ borderTop: 1, borderColor: 'divider' }} style={{position: 'absolute', top: '4rem', left: 0, width: '10vw'}}>
+          {projectsList.map((value, index) => (
+            <Tab value={index} label={value} />
+          ))}
+        </Tabs>
+      </div>
     </div>
   );
 };
